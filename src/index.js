9d65610307eb6610e13efd296e360b23e3e94547
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2025-06-19 01:31:41 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ dotenv.config({
     path : "./env"
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log(error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Database connection failed !! ", err);
+})
+
+
 
 
 /*
