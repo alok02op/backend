ad2b6625888f54a2770a7612f575f61e62edbde2
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2025-06-24 00:07:28 +0530

    add secured routes for user account management: change password, get current user, update account details, and upload images

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,7 +6,14 @@ import {
     registerUser, 
     loginUser, 
     logoutUser, 
-    refreshAccessToken 
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updatedUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 } from "../controllers/user.contollers.js";
 
 const router = Router();
@@ -33,6 +40,20 @@ router.route("/login").post(
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updatedUserAvatar
+);
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router
